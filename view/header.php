0f7ab5e4234ed14ad1c9b389078f6a8472b5171c
ngthhoangkim <nguyenthihoangkim07022004@gmail.com>
2024-03-25 11:39:54 +0700

    tìm kiếm(demo)

--- view/header.php
+++ view/header.php
@@ -149,3 +149,11 @@
     </div>
     <!-- End Header -->
 </body>
+
+<!-- js -->
+<script src="public/js/jquery-1.11.0.min.js"></script>
+    <script src="public/js/jquery-migrate-1.2.1.min.js"></script>
+    <script src="public/js/bootstrap.bundle.min.js"></script>
+    <script src="public/js/templatemo.js"></script>
+    <script src="public/js/custom.js"></script>
+<!-- end js -->
--- view/home.php
+++ view/home.php
@@ -92,10 +92,3 @@
     <!-- End  -->
 </body>
 
-<!-- js -->
-    <script src="public/js/jquery-1.11.0.min.js"></script>
-    <script src="public/js/jquery-migrate-1.2.1.min.js"></script>
-    <script src="public/js/bootstrap.bundle.min.js"></script>
-    <script src="public/js/templatemo.js"></script>
-    <script src="public/js/custom.js"></script>
-<!-- end js -->
