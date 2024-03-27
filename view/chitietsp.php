2ee90009cfe207f6d35d11977a2fd43005dc60cf
codesonkim <son696552@gmail.com>
2024-03-27 19:20:07 +0700

    sữa trang sp và chi tiet

--- view/chitietsp.php
+++ view/chitietsp.php
@@ -1,144 +1,172 @@
 <!DOCTYPE html>
 <html lang="vi">
-<head>
-   <style>
-       .main{
-            border: 1px solid #116A7B;
-            width:100%;
-            height: auto;
-        }
-        .sidebar{
-            border:1px solid #116A7B;
-            height:350px;
-            width:20%;
-            margin-top: 5px;
-            margin-left: 5px;
-            float:left;
-        }
-        ul.list_sidebar{
-            padding:0;
-            margin:0;
-            width:100%;
-            list-style: none;
-            line-height: 35px;
-        }
-        ul.list_sidebar li:hover {
-            background: cadetblue;
-        }
-        ul.list_sidebar li a {
-            text-decoration: none;
-            text-align: left;
-            color:#3333ff;
-            display: block;
-        }
-        ul.list_sidebar li{
-            margin: 7px;
-            padding: 5px;
-        }
-        .product_detail{
-            height:auto;
-            width:79%;
-            margin-top: 5px;
-            margin-left: 5px;
-            float:right;
-        }
-        .product_detail:after {
-         content: "";
-        display: table;
+   <head>
+      <style>
+         body {
+         font-family: Arial, sans-serif;
+         margin: 0;
+         padding: 0;
+         background-color: #f4f4f4;
+         }
+         .container {
+         display: flex;
+         max-width: 1200px;
+         margin: 0 auto;
+         padding: 20px;
+         box-sizing: border-box;
+         }
+         .sidebar {
+         flex: 0 0 20%;
+         background-color: #ffffff;
+         border: 1px solid  #45a049;
+         border-radius: 5px;
+         padding: 10px;
+         margin-right: 20px;
+         }
+         .list_sidebar {
+         padding: 0;
+         margin: 0;
+         list-style: none;
+         }
+         .list_sidebar li {
+         margin-bottom: 10px;
+         }
+         .list_sidebar li a {
+         text-decoration: none;
+         color:  #45a049;
+         display: block;
+         padding: 8px;
+         border-radius: 5px;
+         transition: background-color 0.3s ease;
+         }
+         .list_sidebar li a:hover {
+         background-color: cadetblue;
+         }
+         .product_detail {
+         flex: 1;
+         background-color: #ffffff;
+         border: 1px solid #45a049;
+         border-radius: 5px;
+         padding: 20px;
+         }
+         .hinhanh {
+         width: 40%;
+         float: left;
+         margin-right: 20px;
+         }
+         .hinhanh img {
+         width: 100%;
+         border-radius: 5px;
+         box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+         }
+         .thongtin {
+         width: 50%;
+         float: left;
+         }
+         .thongtin p {
+         margin-top: 0;
+         }
+         .thongtin input[type="submit"] {
+         background-color: #45a049;
+         color: #fff;
+         border: none;
+         padding: 10px 20px;
+         border-radius: 5px;
+         cursor: pointer;
+         transition: background-color 0.3s ease;
+         }
+         .thongtin input[type="submit"]:hover {
+         background-color:  #45a049;
+         }
+         .clear {
          clear: both;
-        }
-        .hinhanh {
-            float:left;
-            width: 40%;
-            height:auto;
-            margin: 5px auto;
-        }
-        .hinhanh img{
-            width:60%;
-        }
-        .thongtin {
-            float:left;
-            width: 60%;
-            height:auto;
-            margin: 5px auto;
-        }
-        
-        .clear{
-            clear:both;
-        }
-   </style>
-</head>
-<body>
-<div class="main">
-    <div class="sidebar">
-        
-        <ul class="list_sidebar">
-            <li><a href="index.php?act=sanpham&id_type=5">Nhẫn</a></li>
-            <li><a href="index.php?act=sanpham&id_type=6">Dây chuyền </a></li>
-            <li><a href="index.php?act=sanpham&id_type=7">Vòng tay </a></li>
-            <li><a href="index.php?act=sanpham&id_type=8">Khuyên tai</a></li>
-        </ul>
-    </div>
-    <div class="product_detail">
-        <h3> Chi tiết sản phẩm </h3>
-        <div class="hinhanh" >
-             <?php
-            // Kết nối cơ sở dữ liệu
-            @include 'connectdb.php';
-
-            // Kiểm tra xem có tham số id_product trên URL hay không
-            if(isset($_GET['id'])){
-                // Lấy giá trị id_product từ URL
-                $id = $_GET['id'];
-                // Truy vấn SQL để lấy thông tin chi tiết sản phẩm
-                $sql = "SELECT * FROM products WHERE id='$id'";
-                $result = $conn->query($sql);
-
-                if ($result->num_rows > 0) {
-                    // Hiển thị thông tin chi tiết sản phẩm
-                    $row_pro = $result->fetch_assoc();
-                    echo '<img src="admin/update_img/'.$row_pro['image'].'" alt="Hình ảnh sản phẩm"> ';
-                } else {
-                    echo 'Không tìm thấy hình ảnh sản phẩm';
-                }
-            } else {
-                echo 'Không có hình ảnh sản phẩm để hiển thị';
-            }
-            ?>
-        </div>
-        <div class="thongtinsp">
-            <?php
-            // Kết nối cơ sở dữ liệu
-            @include 'connectdb.php';
-
-            // Kiểm tra xem có tham số id_product trên URL hay không
-            if(isset($_GET['id'])){
-                // Lấy giá trị id_product từ URL
-                $id = $_GET['id'];
-                // Truy vấn SQL để lấy thông tin chi tiết sản phẩm
-                $sql = "SELECT * FROM products WHERE id='$id'";
-                $result = $conn->query($sql);
-
-                if ($result->num_rows > 0) {
-                    // Hiển thị thông tin chi tiết sản phẩm
-                    $row_pro = $result->fetch_assoc();
-                
-                    echo '<h3>Tên sản phẩm :'.$row_pro['name'].'</h3>';
-                    echo '<p>Giá: '.number_format($row_pro['price'],0,',','.').' đ</p>';
-                    echo '<p>Mô tả: '.$row_pro['method'].'</p>';
-                
-                } else {
-                    echo 'Không tìm thấy sản phẩm';
-                }
-            } else {
-                echo 'Không có thông tin sản phẩm để hiển thị';
-            }
-            ?>
-            <p><input type="submit" value="Thêm giỏ hàng"></p>
-</div>
-
-</div>
-    <div class="clear"></div>
-</div>
-
-</body>
+         }
+         .product_detail h3 ,
+         .product_detail h4 {
+         /* Căn giữa nội dung của thẻ h3 */
+         text-align: center;
+         }
+         .product_detail h3 {
+         padding: 40px;
+         color:  #45a049;
+         }
+      </style>
+   </head>
+   <body>
+      <div class="container">
+         <div class="sidebar">
+            <ul class="list_sidebar">
+               <li><a href="index.php?act=sanpham&id_type=5">Nhẫn</a></li>
+               <li><a href="index.php?act=sanpham&id_type=6">Dây chuyền</a></li>
+               <li><a href="index.php?act=sanpham&id_type=7">Vòng tay</a></li>
+               <li><a href="index.php?act=sanpham&id_type=8">Khuyên tai</a></li>
+            </ul>
+         </div>
+         <div class="product_detail">
+            <h3>CHI TIẾT SẢN PHẨM </h3>
+            <div class="hinhanh">
+               <?php
+                  // Kết nối cơ sở dữ liệu
+                  @include 'connectdb.php';
+                  
+                  // Kiểm tra xem có tham số id_product trên URL hay không
+                  if(isset($_GET['id'])){
+                      // Lấy giá trị id_product từ URL
+                      $id = $_GET['id'];
+                      // Truy vấn SQL để lấy thông tin chi tiết sản phẩm
+                      $sql = "SELECT * FROM products WHERE id='$id'";
+                      $result = $conn->query($sql);
+                  
+                      if ($result->num_rows > 0) {
+                          // Hiển thị thông tin chi tiết sản phẩm
+                          $row_pro = $result->fetch_assoc();
+                          echo '<img src="admin/update_img/'.$row_pro['image'].'" alt="Hình ảnh sản phẩm"> ';
+                      } else {
+                          echo 'Không tìm thấy hình ảnh sản phẩm';
+                      }
+                  } else {
+                      echo 'Không có hình ảnh sản phẩm để hiển thị';
+                  }
+                  ?>
+            </div>
+            <div class="thongtin">
+               <?php
+                  // Kết nối cơ sở dữ liệu
+                  @include 'connectdb.php';
+                  
+                  // Kiểm tra xem có tham số id_product trên URL hay không
+                  if(isset($_GET['id'])){
+                      // Lấy giá trị id_product từ URL
+                      $id = $_GET['id'];
+                      // Truy vấn SQL để lấy thông tin chi tiết sản phẩm
+                      $sql = "SELECT * FROM products WHERE id='$id'";
+                      $result = $conn->query($sql);
+                  
+                      if ($result->num_rows > 0) {
+                          // Hiển thị thông tin chi tiết sản phẩm
+                          $row_pro = $result->fetch_assoc();
+                      
+                          echo '<h4>Tên sản phẩm :'.$row_pro['name'].'</h4>';
+                          echo '<p>Giá: '.number_format($row_pro['price'],0,',','.').' đ</p>';
+                          echo '<p>Mô tả: '.$row_pro['method'].'</p>';
+                      
+                      } else {
+                          echo 'Không tìm thấy sản phẩm';
+                      }
+                  } else {
+                      echo 'Không có thông tin sản phẩm để hiển thị';
+                  }
+                  ?>
+               <?php
+                  echo '<form action="index.php?act=giohang&id" method="post">';
+                  echo '<input type="hidden" name="product_id" value="' . $row_pro['id'] . '">';
+                  echo '<input type="submit" name="add_to_cart" value="Thêm vào giỏ hàng">';
+                  echo '</form>';
+                  
+                             ?>
+            </div>
+         </div>
+         <div class="clear"></div>
+      </div>
+   </body>
+</html>
--- view/sanpham.php
+++ view/sanpham.php
@@ -1,155 +1,216 @@
 <?php
-    @include 'connectdb.php';
-    
-?>
+   // Bao gồm tệp kết nối CSDL
+   @include 'connectdb.php';
+   ?>
 <!DOCTYPE html>
 <html lang="en">
-
-<head>
-    <style>
-        .main{
-            border: 1px solid #116A7B;
-            width:100%;
-            height: auto;
-        }
-        .sidebar{
-            border:1px solid #116A7B;
-            height:350px;
-            width:20%;
-            margin-top: 5px;
-            margin-left: 5px;
-            float:left;
-        }
-        ul.list_sidebar{
-            padding:0;
-            margin:0;
-            width:100%;
-            list-style: none;
-            line-height: 35px;
-        }
-        ul.list_sidebar li:hover {
-            background: cadetblue;
-        }
-        ul.list_sidebar li a {
-            text-decoration: none;
-            text-align: left;
-            color:#3333ff;
-            display: block;
-        }
-        ul.list_sidebar li{
-            margin: 7px;
-            padding: 5px;
-        }
-        .maincontent{
-            height:auto;
-            width:79%;
-            margin-top: 5px;
-            margin-left: 5px;
-            float:right;
-        }
-        ul.product_list{
-            padding: 0;
-            margin: 0;
-            list-style: none;
-            width:100%;
-        }
-        ul.product_list li{
-            width: 19%;
-            border:1px solid  #000;
-            margin: 5px;
-            float:left;
-            background: #79CDCD;
-            height: 330px;
-        }
-        ul.product_list li a {
-            text-decoration: none;
-        }
-        ul.product_list li img {
-            height:190px;
-            width:100%;
-        }
-
-        p.name_product{
-            text-align: center;
-            color:#000;
-            font-size: 8px;
-            font-weight: bold;
-        }
-
-        p.price_product{
-            text-align: center;
-            font-size: 10px;
-            font-weight: bold;
-            color:#000;
-        }
-        .clear{
-            clear:both;
-        }
-
-    </style>
-</head>
-
-<body>
-<div class="main">
-    <div class="sidebar">
-        
-        <ul class="list_sidebar">
-            <li><a href="index.php?act=sanpham&id_type=5">Nhẫn</a></li>
-            <li><a href="index.php?act=sanpham&id_type=6">Dây chuyền </a></li>
-            <li><a href="index.php?act=sanpham&id_type=7">Vòng tay </a></li>
-            <li><a href="index.php?act=sanpham&id_type=8">Khuyên tai</a></li>
-        </ul>
-    </div>
-    <div class="maincontent">
-        
-    <ul class="product_list">
-            <?php 
-             if(isset($_GET['id_type'])){
-    // Lấy giá trị id_type từ URL
-    $id_type = $_GET['id_type'];
-    // Truy vấn SQL để lấy tất cả sản phẩm theo id_type
-    $sql = "SELECT * FROM products WHERE id_type='$id_type'";
-    $result = $conn->query($sql);
-
-    if ($result->num_rows > 0) {
-        // Hiển thị tất cả sản phẩm theo id_type
-        while($row_pro = $result->fetch_assoc()) {
-           
-            echo '<li>';
-            echo '<a href="index.php?act=chitietsp&id='.$row_pro['id'].'">';
-            echo '<img src="admin/update_img/'.$row_pro['image'].'">';
-            echo '<p class="name_product">Tên sản phẩm: '.$row_pro['name'].'</p>';
-            echo '<p class="price_product">Giá: '.number_format($row_pro['price'],0,',','.').' đ</p>';
-            echo '</li>';
-            echo '</a>';
-        }
-    } else {
-        echo 'Không có sản phẩm cho loại này';
-    }
-} else {
-    // Truy vấn SQL để lấy một sản phẩm ngẫu nhiên có id_type
-    $sql = "SELECT * FROM products WHERE id_type IS NOT NULL ORDER BY RAND() LIMIT 10";
-    $result = $conn->query($sql);
-
-    if ($result->num_rows > 0) {
-        // Hiển thị một sản phẩm ngẫu nhiên có id_type
-        while($row_pro = $result->fetch_assoc()) {
-       
-        echo '<li>';
-        echo '<a href="index.php?act=chitietsp&id='.$row_pro['id'].'">';
-        echo '<img src="admin/update_img/'.$row_pro['image'].'">';
-        echo '<p class="name_product">Tên sản phẩm: '.$row_pro['name'].'</p>';
-        echo '<p class="price_product">Giá: '.number_format($row_pro['price'],0,',','.').' đ</p>';
-        echo '</li>';
-        echo '</a>';
-        }
-    } else {
-        echo 'Không có sản phẩm ngẫu nhiên';
-    }
-}
-            ?>
-        </ul>
-    </div>
-    <div class="clear"></div>
-</div>
-</body>
+   <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>Sản Phẩm</title>
+      <style>
+         @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap');
+         * {
+         margin: 0;
+         padding: 0;
+         box-sizing: border-box;
+         font-family: 'Poppins', sans-serif;
+         }
+         body {
+         background-color: #f5f5f5;
+         }
+         .main {
+         max-width: 80%;
+         margin: 0 auto;
+         padding: 20px;
+         display: flex;
+         }
+         .sidebar {
+         background-color: #fff;
+         border-radius: 10px;
+         box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1);
+         padding: 20px;
+         margin-right: 20px;
+         width: 300px;
+         }
+         .sidebar h3 {
+         font-size: 18px;
+         font-weight: 600;
+         margin-bottom: 10px;
+         }
+         .sidebar ul {
+         list-style-type: none;
+         }
+         .sidebar ul li a {
+         display: block;
+         color: #333;
+         padding: 10px;
+         text-decoration: none;
+         border-radius: 5px;
+         transition: background-color 0.3s ease;
+         }
+         .sidebar ul li a:hover {
+         background-color: #f1f1f1;
+         }
+         .maincontent {
+         flex-grow: 1;
+         }
+         .product_list {
+         display: grid;
+         grid-template-columns: repeat(4, 1fr);
+         grid-gap: 20px;
+         }
+         .product_list li {
+         background-color: #fff;
+         border-radius: 10px;
+         box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1);
+         overflow: hidden;
+         transition: transform 0.3s ease;
+         }
+         .product_list li:hover {
+         transform: translateY(-5px);
+         }
+         .product_list li img {
+         width: 100%;
+         height: 200px;
+         object-fit: cover;
+         }
+         .product_info {
+         padding: 20px;
+         }
+         .product_info .name_product {
+         font-size: 16px;
+         font-weight: 600px;
+         margin-bottom: 10px;
+         text-align: left;
+         }
+         .product_info .price_product {
+         font-size: 14px;
+         color: #777;
+         margin-bottom: 10px;
+         text-align: left;
+         }
+         .product_info form {
+         display: flex;
+         justify-content: flex-end;
+         }
+         .product_info form input[type="submit"] {
+         background-color: #4CAF50; /* Green color */
+         color: white;
+         border: none;
+         padding: 10px 20px;
+         border-radius: 5px;
+         cursor: pointer;
+         transition: background-color 0.3s ease;
+         margin-left: 10px; /* Add margin to separate buttons */
+         }
+         .product_info form input[type="submit"]:hover {
+         background-color: #45a049; /* Darker green color on hover */
+         }
+         @media (max-width: 768px) {
+         .main {
+         flex-direction: column;
+         padding: 10px;
+         }
+         .sidebar {
+         width: 100%;
+         margin-right: 0;
+         margin-bottom: 20px;
+         }
+         .product_list {
+         grid-template-columns: repeat(2, 1fr);
+         }
+         }
+         .sidebar ul li a,
+         .product_list li a {
+         text-decoration: none;
+         }
+      </style>
+   </head>
+   <body>
+      <div class="main">
+         <div class="sidebar">
+            <h3>Danh Mục Sản Phẩm</h3>
+            <ul>
+               <li><a href="index.php?act=sanpham&id_type=5">Nhẫn</a></li>
+               <li><a href="index.php?act=sanpham&id_type=6">Dây chuyền</a></li>
+               <li><a href="index.php?act=sanpham&id_type=7">Vòng tay</a></li>
+               <li><a href="index.php?act=sanpham&id_type=8">Khuyên tai</a></li>
+            </ul>
+         </div>
+         <div class="maincontent">
+            <ul class="product_list">
+               <?php
+                  if (isset($_GET['act']) && $_GET['act'] == 'sanpham' && isset($_GET['id_type'])) {
+                      // Lấy giá trị id_type từ URL
+                      $id_type = $_GET['id_type'];
+                      // Truy vấn SQL để lấy tất cả sản phẩm theo id_type
+                      $sql = "SELECT * FROM products WHERE id_type='$id_type'";
+                      $result = $conn->query($sql);
+                  
+                      if ($result->num_rows > 0) {
+                          // Hiển thị tất cả sản phẩm theo id_type
+                          while ($row_pro = $result->fetch_assoc()) {
+                              echo '<li>';
+                              echo '<a href="index.php?act=chitietsp&id=' . $row_pro['id'] . '">';
+                              echo '<img src="admin/update_img/' . $row_pro['image'] . '">';
+                              echo '<div class="product_info">';
+                              echo '<p class="name_product">';
+                  if (strlen($row_pro['name']) > 20) {
+                  echo substr($row_pro['name'], 0, 20) . '...';
+                  } else {
+                  echo $row_pro['name'];
+                  }
+                  echo '</p>';
+                  
+                              echo '<p class="price_product">' . number_format($row_pro['price'], 0, ',', '.') . ' đ</p>';
+                              echo '<form action="index.php?act=giohang&id" method="post">';
+                              echo '<input type="hidden" name="product_id" value="' . $row_pro['id'] . '">';
+                              echo '<input type="submit" name="add_to_cart" value="Thêm vào giỏ hàng">';
+                              echo '</form>';
+                              echo '</div>';
+                              echo '</a>';
+                              echo '</li>';
+                          }
+                      } else {
+                          echo 'Không có sản phẩm cho loại này';
+                      }
+                  } else {
+                      // Truy vấn SQL để lấy một sản phẩm ngẫu nhiên có id_type
+                      $sql = "SELECT * FROM products WHERE id_type IS NOT NULL ORDER BY RAND() LIMIT 12";
+                      $result = $conn->query($sql);
+                  
+                      if ($result->num_rows > 0) {
+                          // Hiển thị một sản phẩm ngẫu nhiên có id_type
+                          while ($row_pro = $result->fetch_assoc()) {
+                              echo '<li>';
+                              echo '<a href="index.php?act=chitietsp&id=' . $row_pro['id'] . '">';
+                              echo '<img src="admin/update_img/' . $row_pro['image'] . '">';
+                              echo '<div class="product_info">';
+                              echo '<p class="name_product">';
+                  if (strlen($row_pro['name']) > 20) {
+                  echo substr($row_pro['name'], 0, 20) . '...';
+                  } else {
+                  echo $row_pro['name'];
+                  }
+                  echo '</p>';
+                  
+                              echo '<p class="price_product">' . number_format($row_pro['price'], 0, ',', '.') . ' đ</p>';
+                              echo '<form action="index.php?act=giohang&id" method="post">';
+                              echo '<input type="hidden" name="product_id" value="' . $row_pro['id'] . '">';
+                              echo '<input type="submit" name="add_to_cart" value="Thêm vào giỏ hàng">';
+                              echo '</form>';
+                              echo '</div>';
+                              echo '</a>';
+                              echo '</li>';
+                          }
+                      } else {
+                          echo 'Không có sản phẩm ngẫu nhiên';
+                      }
+                  }
+                  ?>
+            </ul>
+         </div>
+      </div>
+   </body>
+</html>
